34d7f1f61ccb0940850dd473f85defad722f06e6
Caio Mugarte <caio.mugarte@lumis.com.br>
2024-07-02 13:40:02 -0300

    Wip Simulacao e Mapa Rota

--- components/Mapa.js
+++ components/Mapa.js
@@ -1,10 +1,11 @@
 import L from "leaflet";
+import "leaflet-routing-machine";
 import "leaflet/dist/leaflet.css";
-import React, { useEffect } from "react";
-import { Circle, MapContainer, TileLayer, useMap } from "react-leaflet";
+import React, { useEffect, useRef } from "react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import { StyleSheet, View } from "react-native";
 
-// Configuração do ícone do marker
+// Configuração dos ícones dos marcadores
 const markerIcon = new L.Icon({
   iconUrl: "https://unpkg.com/leaflet@1.6.0/dist/images/marker-icon.png",
   shadowUrl: "https://unpkg.com/leaflet@1.6.0/dist/images/marker-shadow.png",
@@ -14,15 +15,117 @@ const markerIcon = new L.Icon({
   shadowSize: [41, 41],
 });
 
-function MapUpdater({ latitude, longitude }) {
+function MapUpdater({
+  latitude,
+  longitude,
+  produtoLatitude,
+  produtoLongitude,
+}) {
   const map = useMap();
+  const routingControlRef = useRef(null);
+
   useEffect(() => {
+    console.log("MapUpdater - Latitude:", latitude, "Longitude:", longitude);
     map.setView([latitude, longitude], 12); // Centraliza e ajusta o zoom do mapa
-  }, [latitude, longitude, map]);
+
+    const distance = calculateDistance(
+      latitude,
+      longitude,
+      produtoLatitude,
+      produtoLongitude
+    );
+    console.log("Distância calculada:", distance, "km");
+
+    if (distance <= 5) {
+      console.log("Distância <= 5 km, adicionando rota");
+      if (routingControlRef.current) {
+        map.removeControl(routingControlRef.current);
+      }
+      routingControlRef.current = L.Routing.control({
+        waypoints: [
+          L.latLng(latitude, longitude),
+          L.latLng(produtoLatitude, produtoLongitude),
+        ],
+        createMarker: (i, waypoint, n) => {
+          const marker = L.marker(waypoint.latLng, {
+            icon: markerIcon,
+          }).bindTooltip(i === 0 ? "Entregador" : "Local de Entrega", {
+            permanent: true,
+            direction: "top",
+            offset: [0, -25],
+          });
+          return marker;
+        },
+        lineOptions: {
+          styles: [{ color: "#d92830", weight: 4 }],
+        },
+        fitSelectedRoutes: true,
+        show: false,
+        addWaypoints: false,
+        draggableWaypoints: false,
+        routeWhileDragging: true,
+        showAlternatives: false,
+      }).addTo(map);
+
+      // Ajustar o zoom para mostrar toda a rota com menos padding e maxZoom ajustado
+      const bounds = L.latLngBounds([
+        [latitude, longitude],
+        [produtoLatitude, produtoLongitude],
+      ]);
+      map.fitBounds(bounds, { padding: [20, 20], maxZoom: 14 });
+
+      // Remove o container do controle de roteamento
+      const controlContainer = document.querySelector(
+        ".leaflet-routing-container"
+      );
+      if (controlContainer) {
+        controlContainer.style.display = "none";
+      }
+
+      return () => {
+        if (routingControlRef.current) {
+          map.removeControl(routingControlRef.current);
+          routingControlRef.current = null;
+        }
+      };
+    } else {
+      console.log("Distância > 5 km, adicionando círculo");
+      const circle = L.circle([latitude, longitude], { radius: 5000 }).addTo(
+        map
+      );
+
+      return () => {
+        map.removeLayer(circle);
+      };
+    }
+  }, [latitude, longitude, produtoLatitude, produtoLongitude, map]);
+
   return null;
 }
 
-export default function Mapa({ latitude, longitude }) {
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  const toRad = (value) => (value * Math.PI) / 180;
+
+  const R = 6371; // km
+  const dLat = toRad(lat2 - lat1);
+  const dLon = toRad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(lat1)) *
+      Math.cos(toRad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c;
+  return d;
+}
+
+export default function Mapa({
+  latitude,
+  longitude,
+  produtoLatitude,
+  produtoLongitude,
+}) {
   return (
     <View style={styles.container}>
       <link
@@ -41,8 +144,12 @@ export default function Mapa({ latitude, longitude }) {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Circle center={[latitude, longitude]} radius={5000} />
-        <MapUpdater latitude={latitude} longitude={longitude} />
+        <MapUpdater
+          latitude={latitude}
+          longitude={longitude}
+          produtoLatitude={produtoLatitude}
+          produtoLongitude={produtoLongitude}
+        />
       </MapContainer>
     </View>
   );
--- components/SearchPage.js
+++ components/SearchPage.js
@@ -12,126 +12,146 @@ import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Mapa from "./Mapa";
 
-const navigationItems = [
-  { label: "Home", isCurrent: true },
-  { label: "Rentals", isCurrent: false },
-  { label: "Categories", isCurrent: false },
-  { label: "About Us", isCurrent: false },
-  { label: "Contact", isCurrent: false },
-  { label: "Blog", isCurrent: false },
-];
+const getEndereco = async ({ latitude, longitude }) => {
+  const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&addressdetails=1`;
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const MainContent = ({ latitude, longitude }) => (
-  <Box flex={1} bg="#ECECEC" py={4} px={4}>
-    <VStack space={4} alignItems="center" flex={1}>
-      <Box w="100%" maxW="1675px" flex={1}>
-        <HStack space={4} alignItems="flex-start" flex={1}>
-          {latitude != null && longitude != null && (
-            <Box flex={3} height="100%">
-              <Text fontSize="35px" fontWeight="bold">
-                Pedido N° 123123213
-              </Text>
-              <Text fontSize="35px">Localização Atual</Text>
-              <Box flex={1}>
-                <Mapa latitude={latitude} longitude={longitude} />
-              </Box>
-            </Box>
-          )}
-          <VStack flex={2} space={4} mb={4}>
-            <Box
-              bg="#f6f6f6"
-              borderRadius="27px"
-              shadow={2}
-              p={6}
-              alignItems="center"
-            >
-              <Text
-                fontSize="4xl"
-                fontFamily="Plus Jakarta Sans"
-                fontWeight="400"
-                textAlign="center"
-              >
-                Tênis Air Jordan 1 Low:
-              </Text>
-              <Image
-                source={{
-                  uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/7130afe39d51db26199ef55bd9767f491cfc4f715208e4f771e152ab8ef66ee7?apiKey=7cc04bfcf1d94873aadf15ed8bb2db87&",
-                }}
-                alt="Information graphic"
-                style={{ width: "100%", height: 300 }} // Ajuste o valor da altura conforme necessário
-                resizeMode="contain"
-              />
-            </Box>
-            <Box
-              bg="#f6f6f6"
-              borderRadius="27px"
-              shadow={2}
-              p={6}
-              alignItems="center"
-            >
-              <Text
-                fontSize="4xl"
-                fontFamily="Plus Jakarta Sans"
-                fontWeight="400"
-                textAlign="center"
-              >
-                Endereço de Entrega:
-              </Text>
-              <HStack space={2} alignItems="center">
-                <Image
-                  source={{
-                    uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/3d3cb0b07297a456c7c2eee237eb60a83bd7f1643675e58c6a20124f6b115b85?apiKey=7cc04bfcf1d94873aadf15ed8bb2db87&",
-                  }}
-                  alt=""
-                  size="md"
-                  mt={2}
-                />
-                <Text
-                  fontSize="3xl"
-                  fontFamily="Plus Jakarta Sans"
-                  fontWeight="400"
-                >
-                  Brasília, DF
-                </Text>
-              </HStack>
-            </Box>
-            <Box
-              bg="#f6f6f6"
-              borderRadius="27px"
-              shadow={2}
-              p={6}
-              alignItems="center"
-            >
-              <Text
-                fontSize="4xl"
-                fontFamily="Plus Jakarta Sans"
-                fontWeight="400"
-                textAlign="center"
-              >
-                Última cidade em que passou:
-              </Text>
-              <HStack space={2} alignItems="center">
-                <Image
-                  source={{
-                    uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/9370af00c739fc5e5ba6235baa520fa160c5268983d57cd4d5e5f4868475ed57?apiKey=7cc04bfcf1d94873aadf15ed8bb2db87&",
-                  }}
-                  alt=""
-                  size="md"
-                />
-                <Text
-                  fontSize="3xl"
-                  fontFamily="Plus Jakarta Sans"
-                  fontWeight="400"
-                >
-                  Brasília, DF
-                </Text>
+const MainContent = ({
+  latitude,
+  longitude,
+  enderecoEntrega,
+  lastCity,
+  entregadorLatitude,
+  entregadorLongitude,
+}) => (
+  <>
+    {latitude != null &&
+      longitude != null &&
+      enderecoEntrega != null &&
+      lastCity != null && (
+        <Box flex={1} bg="#ECECEC" py={4} px={4}>
+          <VStack space={4} alignItems="center" flex={1}>
+            <Box w="100%" maxW="1675px" flex={1}>
+              <HStack space={4} alignItems="flex-start" flex={1}>
+                <Box flex={3} height="100%">
+                  <Text fontSize="35px" fontWeight="bold">
+                    Pedido N° 123123213
+                  </Text>
+                  <Text fontSize="35px">Localização Atual</Text>
+                  <Box flex={1}>
+                    <Mapa
+                      latitude={latitude}
+                      longitude={longitude}
+                      entregadorLatitude={entregadorLatitude}
+                      entregadorLongitude={entregadorLongitude}
+                    />
+                  </Box>
+                </Box>
+
+                <VStack flex={2} space={4} mb={4}>
+                  <Box
+                    bg="#f6f6f6"
+                    borderRadius="27px"
+                    shadow={2}
+                    p={6}
+                    alignItems="center"
+                  >
+                    <Text
+                      fontSize="4xl"
+                      fontFamily="Plus Jakarta Sans"
+                      fontWeight="400"
+                      textAlign="center"
+                    >
+                      Tênis Air Jordan 1 Low:
+                    </Text>
+                    <Image
+                      source={{
+                        uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/7130afe39d51db26199ef55bd9767f491cfc4f715208e4f771e152ab8ef66ee7?apiKey=7cc04bfcf1d94873aadf15ed8bb2db87&",
+                      }}
+                      alt="Information graphic"
+                      style={{ width: "100%", height: 300 }} // Ajuste o valor da altura conforme necessário
+                      resizeMode="contain"
+                    />
+                  </Box>
+                  <Box
+                    bg="#f6f6f6"
+                    borderRadius="27px"
+                    shadow={2}
+                    p={6}
+                    alignItems="center"
+                  >
+                    <Text
+                      fontSize="4xl"
+                      fontFamily="Plus Jakarta Sans"
+                      fontWeight="400"
+                      textAlign="center"
+                    >
+                      Endereço de Entrega:
+                    </Text>
+                    <HStack space={2} alignItems="center">
+                      <Image
+                        source={{
+                          uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/3d3cb0b07297a456c7c2eee237eb60a83bd7f1643675e58c6a20124f6b115b85?apiKey=7cc04bfcf1d94873aadf15ed8bb2db87&",
+                        }}
+                        alt=""
+                        size="md"
+                        mt={2}
+                      />
+                      <Text
+                        fontSize="3xl"
+                        fontFamily="Plus Jakarta Sans"
+                        fontWeight="400"
+                      >
+                        {enderecoEntrega}
+                      </Text>
+                    </HStack>
+                  </Box>
+                  <Box
+                    bg="#f6f6f6"
+                    borderRadius="27px"
+                    shadow={2}
+                    p={6}
+                    alignItems="center"
+                  >
+                    <Text
+                      fontSize="4xl"
+                      fontFamily="Plus Jakarta Sans"
+                      fontWeight="400"
+                      textAlign="center"
+                    >
+                      Última cidade em que passou:
+                    </Text>
+                    <HStack space={2} alignItems="center">
+                      <Image
+                        source={{
+                          uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/9370af00c739fc5e5ba6235baa520fa160c5268983d57cd4d5e5f4868475ed57?apiKey=7cc04bfcf1d94873aadf15ed8bb2db87&",
+                        }}
+                        alt=""
+                        size="md"
+                      />
+                      <Text
+                        fontSize="3xl"
+                        fontFamily="Plus Jakarta Sans"
+                        fontWeight="400"
+                      >
+                        {lastCity}
+                      </Text>
+                    </HStack>
+                  </Box>
+                </VStack>
               </HStack>
             </Box>
           </VStack>
-        </HStack>
-      </Box>
-    </VStack>
-  </Box>
+        </Box>
+      )}
+  </>
 );
 
 const Footer = () => (
@@ -151,6 +171,10 @@ export default function SearchPage({ navigation, route }) {
   const { codigoRastreio } = route.params;
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
+  const [enderecoEntrega, setEnderecoEntrega] = useState(null);
+  const [lastCity, setLastCity] = useState(null);
+  const [produtoLatitude, setProdutoLatitude] = useState(null);
+  const [produtoLongitude, setProdutoLongitude] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -163,8 +187,30 @@ export default function SearchPage({ navigation, route }) {
             },
           }
         );
-        setLatitude(response.data.localizacao.latitude);
-        setLongitude(response.data.localizacao.longitude);
+        const latitude = response.data.localizacao.latitude;
+        const longitude = response.data.localizacao.longitude;
+
+        const enderecoEntrega = await getEndereco({
+          latitude: response.data.produto.localEntrega.latitude,
+          longitude: response.data.produto.localEntrega.longitude,
+        });
+        const enderecoEntregador = await getEndereco({
+          latitude: latitude,
+          longitude: longitude,
+        });
+        setLatitude(latitude);
+        setLongitude(longitude);
+        setProdutoLatitude(response.data.produto.localEntrega.latitude);
+        setProdutoLongitude(response.data.produto.localEntrega.longitude);
+        setEnderecoEntrega(
+          `${enderecoEntrega.address.road}, ${enderecoEntrega.address.city}`
+        );
+        setLastCity(
+          `${enderecoEntregador.address.city}, ${
+            enderecoEntregador.address["ISO3166-2-lvl4"].split("-")[1]
+          }`
+        );
+
         console.log(
           `SetLatitude: ${response.data.localizacao.latitude} and SetLongitude: ${response.data.localizacao.longitude}`
         );
@@ -182,7 +228,14 @@ export default function SearchPage({ navigation, route }) {
       <Box flex={1} bg="white">
         <Header navigation={navigation} />
         <Box flex={1} bg="#ECECEC">
-          <MainContent latitude={latitude} longitude={longitude} />
+          <MainContent
+            latitude={latitude}
+            longitude={longitude}
+            enderecoEntrega={enderecoEntrega}
+            lastCity={lastCity}
+            produtoLatitude={produtoLatitude}
+            produtoLongitude={produtoLongitude}
+          />
         </Box>
         <Footer />
       </Box>
